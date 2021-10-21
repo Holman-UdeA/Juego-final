79612c1d75e08af4f8aac3b80e05bba7f1af7514
Holman <holman.londono@udea.edu.co>
2021-10-20 19:38:33 -0500

    Inicio a pasar codigo Inicie a escribir codigo del programa de preuba dentro del juego final, esto por lo que mencione en el commit anterior, no copio y pego simplemente porque el codigo del programa de prueba tiene algunos fallos simples, entonces así lo voy corrigiendo.

--- Codificacion/Game/Chaos_Night/mainwindow.cpp
+++ Codificacion/Game/Chaos_Night/mainwindow.cpp
@@ -6,10 +6,43 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    SetUp_MainWindow();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::SetUp_MainWindow()
+{
+    Archivo = new fstream;
+    Escena = new QGraphicsScene;
+    AnchoEsc = 1000;
+    AltoEsc = 500;
+    ui->graphicsView->setGeometry(0, 0, AnchoEsc, AltoEsc);
+    Escena->setSceneRect(0, 0, AnchoEsc-2, AltoEsc-2);
+    setFixedSize(AnchoEsc+2, AltoEsc+25);
+    ui->PButton_Ingresar->setGeometry((AnchoEsc/2)-111, AltoEsc/2, 101, 25);
+    ui->PButton_Registrar->setGeometry((AnchoEsc/2)+10, AltoEsc/2, 101, 25);
+    ui->LabelUser->setGeometry((AnchoEsc/2)-111, (AltoEsc/2)-65, 85, 25);
+    ui->Label_Password->setGeometry((AnchoEsc/2)-111, (AltoEsc/2)-35, 85, 25);
+    ui->LEdit_User->setGeometry((AnchoEsc/2)-9, (AltoEsc/2)-65, 120, 25);
+    ui->LEdit_Password->setGeometry((AnchoEsc/2)-9, (AltoEsc/2)-35, 120, 25);
+}
+
+void MainWindow::GetUsers()
+{
+
+}
+
+void MainWindow::Hide_Login()
+{
+
+}
+
+bool MainWindow::CheckAccount(int Opcion)
+{
+
 }
 
--- Codificacion/Game/Chaos_Night/mainwindow.h
+++ Codificacion/Game/Chaos_Night/mainwindow.h
@@ -2,10 +2,18 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <fstream>
+#include <string>
+#include <QMessageBox>
+#include <QGraphicsScene>
+
+#define TxtPath "../Chaos_Night/Users.txt"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+using namespace std;
 
 class MainWindow : public QMainWindow
 {
@@ -17,5 +25,14 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    fstream *Archivo;
+    QGraphicsScene *Escena;
+    int AnchoEsc, AltoEsc;
+    unsigned long PosUser;
+    string Usuario, Password, Users;
+    void SetUp_MainWindow();
+    void GetUsers();
+    void Hide_Login();
+    bool CheckAccount(int Opcion);
 };
 #endif // MAINWINDOW_H
